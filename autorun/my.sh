9dce7311599b111a8e4944f70d48eeec19231a3c
Li Feng <fengli@smartx.com>
2022-09-15 15:56:33 +0800

    add my
    Signed-off-by: Li Feng <fengli@smartx.com>

---
+++ autorun/my.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# SPDX-License-Identifier: (LGPL-2.1 OR LGPL-3.0)
+# Copyright (C) SUSE LLC 2017, all rights reserved.
+
+_vm_ar_env_check || exit 1
+
+modprobe virtio_blk 2>/dev/null
+modprobe virtio_scsi 2>/dev/null
+mkdir /root/shared 2>/dev/null
+mount -t 9p -o trans=virtio fs0 /root/shared 2>/dev/null
+mount -t debugfs debugfs /sys/kernel/debug/ 2>/dev/null
+ip link set lo up 2>/dev/null
+
+cd "/root" || _fatal
+return
---
+++ cut/my.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# SPDX-License-Identifier: (LGPL-2.1 OR LGPL-3.0)
+# Copyright (C) SUSE LLC 2017, all rights reserved.
+
+RAPIDO_DIR="$(realpath -e ${0%/*})/.."
+. "${RAPIDO_DIR}/runtime.vars"
+
+_rt_require_dracut_args "$RAPIDO_DIR/autorun/my.sh" "$@"
+#_rt_mem_resources_set "2048M"
+#_rt_require_networking
+#  yum install systemd-networkd
+
+"$DRACUT" --install "netstat sysctl lsmod ip ping lspci lsscsi zcat \
+           tail blockdev ps rmdir resize dd vim grep find df sha256sum \
+		   getopt tput wc column blktrace losetup parted truncate \
+		   lsblk strace which awk bc touch cut chmod true false mktemp \
+		   killall id sort uniq date expr tac diff head dirname seq \
+		   basename tee egrep hexdump sync fio logger cmp stat nproc \
+		   xfs_io modinfo blkdiscard realpath timeout nvme" \
+    --include "/usr/lib64/fio/fio-libaio.so" "/usr/lib64/fio/fio-libaio.so" \
+	--modules "base" \
+	"${DRACUT_RAPIDO_ARGS[@]}" \
+	"$DRACUT_OUT" || _fail "dracut failed"
+	#--add-drivers "zram lzo lzo-rle scsi_debug null_blk loop nvme" \
